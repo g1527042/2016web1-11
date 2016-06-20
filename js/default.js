42384213626de7d9eeb438623b3cbf4f236f8a60
g1527042 <g1527042@st.t-kougei.ac.jp>
2016-06-20 17:38:47 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,3 +1,21 @@
+// 答えを用意
+var number = Math.floor(Math.random()*3);
+var answer = parseInt(window.prompt('数当てゲーム。0～2の数字を入力してください！'));
+
+var message;
+if(answer === number){
+  message = 'あたり';
+}else if(answer < number){
+  message = '残念！もっと大きいです';
+}else if(number < answer){
+  message = '残念！もっと小さいです！';
+}else{
+  message = '0～3の数字を入力してください';
+}
+
+// 結果を表示
+document.getElementById('choice').textContent = message;
+
 // 確認ダイアログを表示
 var result = window.confirm('準備OK？');
 // 結果を表示
