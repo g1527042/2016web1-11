9825aca31950dfbaea4d793fd8e4eeba6605d449
g1527042 <g1527042@st.t-kougei.ac.jp>
2016-06-20 17:25:34 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -2,8 +2,7 @@
 ver result = window.confirm('準備OK？');
 // 結果を表示
 document.getElementById('choice').textContent = result;
-// 日時を表示
-document.getElementById('choice').textContent = new Date();
+
 /* ダイアログを表示 */
 window.alert('こんにちは世界！');
 // コンソールに表示
